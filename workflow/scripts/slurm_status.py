a31c33172b3b8dac105ddad51b9a851b008d8602
EisenRa <raph.eisenhofer@gmail.com>
2023-06-07 09:53:38 +0200

    Add 'TIMEOUT' to list of valid sacct State values

--- workflow/scripts/slurm_status.py
+++ workflow/scripts/slurm_status.py
@@ -6,7 +6,7 @@ jobid = sys.argv[-1]
 
 output = str(subprocess.check_output("sacct -j %s --format State --noheader | head -1 | awk '{print $1}'" % jobid, shell=True).strip())
 
-running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED", "PREEMPTED"]
+running_status=["PENDING", "CONFIGURING", "COMPLETING", "RUNNING", "SUSPENDED", "PREEMPTED", "TIMEOUT"]
 if "COMPLETED" in output:
   print("success")
 elif any(r in output for r in running_status):
