74b47a4e3fff3e2cebb7ee1916d3025eea663c3b
EisenRa <raph.eisenhofer@gmail.com>
2023-07-04 15:25:27 +0200

    fix issue with API requests (can't pull more than 100 by default, so implemente offsetting) It's not super fast, so in the future there may have to be a better implementation.

--- workflow/airtable/get_derep_mags_airtable.py
+++ workflow/airtable/get_derep_mags_airtable.py
@@ -17,7 +17,7 @@ with open('/projects/ehi/data/.airtable_api_key.json') as f:
 api_key = config['api_key']
 
 #Set variables
-url = 'https://api.airtable.com/v0/appWbHBNLE6iAsMRV/tblMzd3oyaJhdeQcs'
+base_url = 'https://api.airtable.com/v0/appWbHBNLE6iAsMRV/tblMzd3oyaJhdeQcs'
 headers = {
     'Authorization': f'Bearer {api_key}',
     'Content-Type': 'application/json'
@@ -26,16 +26,11 @@ headers = {
 # Set up the query parameters to filter the records
 query_params = {
     'filterByFormula': f"AND(FIND('{args.dmb}', {{DM_batch}}), {{dereplicated}} = 'true', {{annotated}} = 'false')",
+    'pageSize': 100  # Set the page size to 100 records per request
 }
 
 # Make the request to get the records
-response = requests.get(url, params=query_params, headers=headers)
-
-# Convert the response to a JSON object
-data = response.json()
-
-# Extract the records from the JSON object
-records = data['records']
+response = requests.get(base_url, params=query_params, headers=headers)
 
 # Set up the output TSV file
 output_file_path = 'dereped_mags.csv'
@@ -44,18 +39,39 @@ with open(output_file_path, 'w', newline='') as tsvfile:
     writer = csv.writer(tsvfile, delimiter=',')
     writer.writerow(['ehm', 'mag_name'])
 
+    offset = None
+    while True:
+        # Update the query parameters with the offset if it exists
+        if offset:
+            query_params['offset'] = offset
+
+
+        # Make the request to get the records
+        response = requests.get(base_url, params=query_params, headers=headers)
+
+        # Convert the response to a JSON object
+        data = response.json()
+
+        # Extract the records from the JSON object
+        records = data['records']
+
     # Loop through the records and write each one to the csv file
-    for record in records:
-        # Get the ID of the record
-        record_id = record['id']
+        for record in records:
+            # Get the ID of the record
+            record_id = record['id']
 
-        # Make a request to get the record data
-        record_response = requests.get(f"{url}/{record_id}", headers=headers)
+            record_response = requests.get(f"{base_url}/{record_id}", headers=headers)
 
-        # Extract the values of the completeness and contamination fields
-        ehm = record_response.json()['fields'].get('ID', '')
-        mag_name = record_response.json()['fields'].get('mag_name', '')
+            # Extract the values of the completeness and contamination fields
+            ehm = record_response.json()['fields'].get('ID', '')
+            mag_name = record_response.json()['fields'].get('mag_name', '')
 
-        # Write the row to the csv file
-        row = [ehm, mag_name]
-        writer.writerow(row)
+            # Write the row to the csv file
+            row = [ehm, mag_name]
+            writer.writerow(row)
+
+        # Check if there are more records to retrieve
+        if 'offset' in data:
+            offset = data['offset']
+        else:
+            break
--- workflow/airtable/get_mags_airtable.py
+++ workflow/airtable/get_mags_airtable.py
@@ -1,23 +1,21 @@
-## Raphael Eisenhofer 2023
-
 import argparse
 import csv
 import pandas as pd
 import requests
 import json
 
-#Load in CSV file
+# Load CSV file
 parser = argparse.ArgumentParser()
 parser.add_argument('--dmb', required=True, help='DMB number')
 args = parser.parse_args()
 
-#Read the API key from config file
+# Read the API key from the config file
 with open('/projects/ehi/data/.airtable_api_key.json') as f:
     config = json.load(f)
 api_key = config['api_key']
 
-#Set variables
-url = 'https://api.airtable.com/v0/appWbHBNLE6iAsMRV/tblMzd3oyaJhdeQcs'
+# Set variables
+base_url = 'https://api.airtable.com/v0/appWbHBNLE6iAsMRV/tblMzd3oyaJhdeQcs'
 headers = {
     'Authorization': f'Bearer {api_key}',
     'Content-Type': 'application/json'
@@ -25,18 +23,9 @@ headers = {
 
 # Set up the query parameters to filter the records
 query_params = {
-    'filterByFormula': f"{{DM_batch}} = '{args.dmb}'",
-
+    'filterByFormula': f"FIND('{args.dmb}', {{DM_batch}})",
+    'pageSize': 100  # Set the page size to 100 records per request
 }
-
-# Make the request to get the records
-response = requests.get(url, params=query_params, headers=headers)
-
-# Convert the response to a JSON object
-data = response.json()
-
-# Extract the records from the JSON object
-records = data['records']
 
 # Set up the output TSV file
 output_file_path = 'mags.csv'
@@ -45,19 +34,40 @@ with open(output_file_path, 'w', newline='') as tsvfile:
     writer = csv.writer(tsvfile, delimiter=',')
     writer.writerow(['genome', 'completeness', 'contamination'])
 
-    # Loop through the records and write each one to the csv file
-    for record in records:
-        # Get the ID of the record
-        record_id = record['id']
+    offset = None
+    while True:
+        # Update the query parameters with the offset if it exists
+        if offset:
+            query_params['offset'] = offset
 
-        # Make a request to get the record data
-        record_response = requests.get(f"{url}/{record_id}", headers=headers)
+        # Make the request to get the records
+        response = requests.get(base_url, params=query_params, headers=headers)
 
-        # Extract the values of the completeness and contamination fields
-        mag_name = record_response.json()['fields'].get('mag_name', '')
-        completeness = record_response.json()['fields'].get('completeness', '')
-        contamination = record_response.json()['fields'].get('contamination', '')
+        # Convert the response to a JSON object
+        data = response.json()
 
-        # Write the row to the csv file
-        row = [mag_name, completeness, contamination]
-        writer.writerow(row)
+        # Extract the records from the JSON object
+        records = data['records']
+
+        # Loop through the records and write each one to the CSV file
+        for record in records:
+            # Get the ID of the record
+            record_id = record['id']
+
+            # Make a request to get the record data
+            record_response = requests.get(f"{base_url}/{record_id}", headers=headers)
+
+            # Extract the values of the completeness and contamination fields
+            mag_name = record_response.json()['fields'].get('mag_name', '')
+            completeness = record_response.json()['fields'].get('completeness', '')
+            contamination = record_response.json()['fields'].get('contamination', '')
+
+            # Write the row to the CSV file
+            row = [mag_name, completeness, contamination]
+            writer.writerow(row)
+
+        # Check if there are more records to retrieve
+        if 'offset' in data:
+            offset = data['offset']
+        else:
+            break
