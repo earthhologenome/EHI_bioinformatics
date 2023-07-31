e4e27baab35065cc6350904c41e6923783a3e1df
EisenRa <raph.eisenhofer@gmail.com>
2023-07-31 15:53:18 +0200

    rewrite summarise for checkm2

--- workflow/scripts/metawrap_summarise_checkm.py
+++ workflow/scripts/metawrap_summarise_checkm.py
@@ -1,47 +1,28 @@
-#!/usr/bin/env python2.7
-from __future__ import print_function
+## Raphael Eisenhofer 2023: I've rewritten this to work with Checkm2 output
+
 import sys
-# This script summarizes the statistics of each bin by parsing 
-# the checkm_folder/storage/bin_stats_ext.tsv file of the CheckM output
 
-## Raphael Eisenhofer 2023: I've updated this to work with CheckM2
-
-if len(sys.argv)==3: 
-	binner=sys.argv[2]
-	print("bin\tcompleteness\tcontamination\tGC\tlineage\tN50\tsize\tbinner")
-elif len(sys.argv)==4:
-	source={}
-	for line in open(sys.argv[3]):
-		cut=line.strip().split("\t")
-		source[cut[0]]=cut[7]
-	print("bin\tcompleteness\tcontamination\tGC\tlineage\tN50\tsize\tbinner")
+if len(sys.argv) == 2:
+    print("bin\tcompleteness\tcontamination\tGC\tlineage\tN50\tcoding_density\tsize")
 else:
-	print("bin\tcompleteness\tcontamination\tGC\tlineage\tN50\tsize")
-
+    print("bin\tcompleteness\tcontamination\tGC\tlineage\tN50\tcoding_density\tsize\tbinner")
 
 for line in open(sys.argv[1]):
-	dic=eval(line.strip().split("\t")[1])
+    if line.startswith("Name"):
+        continue  # Skip the header line
+    cols = line.strip().split("\t")
+    name = cols[0]
+    completeness = float(cols[1])
+    contamination = float(cols[2])
+    gc = float(cols[9])
+    lineage = cols[11]
+    n50 = int(cols[6])
+    size = int(cols[8])
+    coding_density = float(cols[5])
 
-	#if dic["Completeness"]<20 or dic["Contamination"]>10: continue
-	if "__" in dic["marker lineage"]: dic["marker lineage"]=dic["marker lineage"].split("__")[1]
-	#if "unbinned" in line.split("\t")[0]: name="Unbinned"
-	name=line.split("\t")[0]
 
-
-	if len(sys.argv)==3:	
-		print("\t".join([name, str(dic["Completeness"])[:5],\
-		 str(dic["Contamination"])[:5], str(dic["GC"])[:5],\
-		 dic["marker lineage"], str(dic["N50 (contigs)"]),\
-		 str(dic["Genome size"]), binner]))
-
-	elif len(sys.argv)==4:
-		print("\t".join([name, str(dic["Completeness"])[:5],\
-		 str(dic["Contamination"])[:5], str(dic["GC"])[:5],\
-		 dic["marker lineage"], str(dic["N50 (contigs)"]),\
-		 str(dic["Genome size"]), source[name]]))
-
-	else:
-		print("\t".join([name, str(dic["Completeness"])[:5],\
-		 str(dic["Contamination"])[:5], str(dic["GC"])[:5],\
-		 dic["marker lineage"], str(dic["N50 (contigs)"]),\
-		 str(dic["Genome size"])]))
+    if len(sys.argv) == 2:
+        print("\t".join([name, str(completeness)[:5], str(contamination)[:5], str(gc)[:5], lineage, str(n50), str(coding_density)[:5], str(size)]))
+    else:
+        binner=sys.argv[2]
+        print("\t".join([name, str(completeness)[:5], str(contamination)[:5], str(gc)[:5], lineage, str(n50), str(coding_density)[:5], str(size), binner]))
